15bdddc2bfed2dc7f67e1ec78062e7ffda11466e
imxblee <transcendencez@gmail.com>
2015-12-21 12:18:32 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Carsale::Application.config.secret_key_base = '45f8254f74afa36ece57976fbea8410a8d29cf4de7225712a024cc51dd86f1a72250f6d6f4c04194461bbf32b687589e34b123038db73a2788cb6da09c2f7161'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Carsale::Application.config.session_store :cookie_store, key: '_carsale_session'
